cf908ffcc5bc3e93d5cd698412b144c26508db69
Will Worrachatr Phoonbamphen <46480811+willphoonbamphen@users.noreply.github.com>
2022-08-07 13:38:39 +0700

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://81019ce5.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
